1f0d96ea945d6fa004f939e7999b696a955a7df4
Hyperion <pvega.impulsa@gmail.com>
2021-02-07 12:26:48 +0100

    Aerolinea 2019 v1

---
+++ flights/views.py
@@ -0,0 +1,41 @@
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+from .models import Flight, Passenger
+
+# Create your views here.
+
+def index(request):
+    context = {
+        "flights": Flight.objects.all()
+    }
+    return render(request, "flights/index.html", context)
+
+def flight(request, flight_id):
+    try:
+        flight = Flight.objects.get(pk=flight_id)
+    except Flight.DoesNotExist:
+        raise Http404("Este vuelo no existe.")
+    context = {
+        "flight": flight,
+        "passengers": flight.passengers.all(),
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
+    }
+    return render (request, "flights/flight.html", context)
+
+def book(request, flight_id):
+    try:
+        passenger_id = int(request.POST["passenger"])
+        passenger = Passenger.objects.get(pk=passenger_id)
+        flight = Flight.objects.get(pk=flight_id)
+    except KeyError:
+        return render(request, "flights/error.html", {"message": "No seleccionaste ningun pasajero."})
+    except Passenger.DoesNotExist:
+        return render(request, "flights/error.html", {"message": "El pasajero no existe."})
+    except Flight.DoesNotExist:
+        return render(request, "flights/index.html", {"message": "El vuelo no existe."})
+
+    passenger.flights.add(flight)
+
+    return HttpResponseRedirect(reverse("flight", args=(flight_id,)))
